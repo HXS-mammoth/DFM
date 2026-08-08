ab55ddfba5d489a918852509531e22b370b612f7
HXS-mammoth <l2890918071@gmail.com>
2026-08-08 22:27:37 +0800

    创建 customize.sh

---
+++ customize.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+ui_print " "
+ui_print "  ★ DFM Block Pro v2.0 (Ace6) ★"
+ui_print "  三角洲行动恶意文件 + hosts阻断"
+ui_print "  更激进清理 + 开机自启动"
+ui_print " "
+ui_print "  目标: com.tencent.tmgp.dfm"
+ui_print "  功能: 文件清理 + 域名阻断 + 目录锁"
+ui_print " "
